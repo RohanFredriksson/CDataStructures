4c77e6bea43bdf43d889dcc6fa150844b47f4b58
RohanFredriksson <rfre3327@uni.sydney.edu.au>
2022-11-10 23:24:15 +1100

    Building the test cases is now optional.

--- build.sh
+++ build.sh
@@ -10,7 +10,7 @@ printf " Creating Makefile\n"
 printf "=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n"
 printf "\n"
 
-cmake ..
+cmake .. -DDO_TESTING=1
 if [ $? != 0 ]; then
     exit 1
 fi
